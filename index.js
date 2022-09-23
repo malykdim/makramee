79387abfde50eba35ebd41a05239a8aae8135f59
Albena Kostova <albena.kostov@gmail.com>
2022-09-23 22:23:36 +0200

    App Initialization

--- index.js
+++ index.js
@@ -1,37 +1,15 @@
-const http = require('http');
-
-const html = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Makramee</title>
-</head>
-<body>
-    <h1>Makramee</h1>
-</body>
-</html>
-`;
-
-const server = http.createServer((req, res) => {
-    console.log(`Request received`);
-    console.log(req.method); 
-    console.log(req.headers); 
-    console.log(req.url); 
-    
-    if (req.url == '/') {
-                
-        res.write(html);
-        
-        res.end();        
-    } else {
-        res.statusCode = 404;
-        res.write('404 page');
-        res.end();
-    }
-    
+const express = require('express');
+const hbs = require('express-handlebars').create({
+    extname: '.hbs'
 });
 
-server.listen(3000);
+const app = express();
+
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('static'));
+
+
+app.listen(3000, () => console.log('Makramee listening on port 3000'));
