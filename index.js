ddc10d2afe3821f0f7ae850a64fa0e71318a9ba7
Albena Kostova <albena.kostov@gmail.com>
2022-09-24 15:47:04 +0200

    Home functionality

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const hbs = require('express-handlebars').create({
     extname: '.hbs'
 });
 
+const homeController = require('./controllers/homeController');
+
 const app = express();
 
 app.engine('.hbs', hbs.engine);
@@ -11,5 +13,6 @@ app.set('view engine', '.hbs');
 app.use(express.urlencoded({ extended: true }));
 app.use('/static', express.static('static'));
 
+app.use(homeController);
 
 app.listen(3000, () => console.log('Makramee listening on port 3000'));
