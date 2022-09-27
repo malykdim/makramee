5c724c4b3837a9a08f139f3fa6250cff748194d8
Albena Kostova <albena.kostov@gmail.com>
2022-09-27 15:32:54 +0200

    Create forme & functionality

--- controllers/createController.js
+++ controllers/createController.js
@@ -1,3 +1,5 @@
+const { create } = require('../services/dataService');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
@@ -6,5 +8,16 @@ router.get('/', (req, res) => {
     });
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const result = await create(req.body);
+        res.redirect('/catalog/' + result.id);
+    } catch (err) {
+        res.render('create', {
+            title: 'Request Error'
+        });
+        console.log(err.message);        
+    }
+});
 
 module.exports = router;
--- services/dataService.js
+++ services/dataService.js
@@ -1,8 +1,32 @@
 const fs = require('fs');
 
 const fileName = './models/data.json';
-
 const data = JSON.parse(fs.readFileSync(fileName));
+
+function getAll() {
+    return data;
+}
+
+function getById(id) {
+    return data.find(item => item.id == id);
+}
+
+async function create(itemData) {
+    const item = {
+        id: getId(),
+        imgURL: itemData.imgURL,
+        name: itemData.name,
+        price: Number(itemData.price),
+        category: itemData.category,
+        author: itemData.author,
+        description: itemData.description
+    }
+    
+    data.push(item);
+    await persist();
+    return item;
+}
+
 
 async function persist() {
     return new Promise((res, rej) => {
@@ -16,15 +40,12 @@ async function persist() {
     });
 }
 
-function getAll() {
-    return data;
-}
-
-function getById(id) {
-    return data.find(item => item.id == id);
+function getId() {
+    return ('000000' + (Math.random() * 999999 | 0).toString(16).slice(-6));
 }
 
 module.exports = {
     getAll,
-    getById
+    getById,
+    create
 }
