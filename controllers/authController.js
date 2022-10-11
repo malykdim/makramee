186ea3b3227740e049288dad9c493b64c0b7ecd0
Albena Kostova <albena.kostov@gmail.com>
2022-10-11 21:56:28 +0200

    authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
 const authController = require('express').Router();
+const { body, validationResult } = require('express-validator');
 const { login, register } = require('../services/authService');
+const { parseError } = require('../utils/parser');
+
 
 /* /signin */
 authController.get('/signin', (req, res) => {
@@ -8,18 +11,25 @@ authController.get('/signin', (req, res) => {
     });
 });
 
-authController.post('/signin', async (req, res) => {
-    try {
-        const result = await login(req.body.username, req.body.password);
-        attachToken(req, res, result);
-        res.redirect('/catalog');
-    } catch (error) {
-        res.render('signIn', {
-            title: 'Sign in',
-            error: error.message.split('\n')
-        });
-    }
-});
+authController.post(
+    '/signin', 
+    body(['username', 'password']).trim(),
+    async (req, res) => {
+        try {
+            const result = await login(req.body.username, req.body.password);
+            attachToken(req, res, result);
+            res.redirect('/catalog');
+        } catch (error) {
+            res.render('signIn', {
+                title: 'Sign in',
+                body: {
+                    username: req.body.username
+                },
+                error: parseError(error)
+            });
+        }
+}
+);
 
 
 /* /signup */
@@ -29,24 +39,44 @@ authController.get('/signup', (req, res) => {
     });
 });
 
-authController.post('/signup', async (req, res) => {
-    try {
-        if (req.body.username.trim() == '' || req.body.password.trim() == '') {
-            throw new Error('All fields are required!');
+authController.post(
+    '/signup', 
+    body('username')
+        .trim()
+        .notEmpty().withMessage('Username is required').bail()
+        .isAlphanumeric().withMessage('Username may contain only english letters and numbers'),
+    body('password')
+        .trim()
+        .isLength({ min: 3 }).withMessage('Password must be at least 3 characters long'),
+    body('repass')
+        .trim()
+        .custom(async (value, { req }) => {
+            if (value != req.body.password) {
+                throw new Error(`Passwords don't match`);
+            }
+        }),
+    async (req, res) => {
+        try {
+            const { errors } = validationResult(req);
+            
+            if (errors.length > 0) {
+                throw errors;
+            }
+            
+            const result = await register(req.body.username, req.body.password);    
+            attachToken(req, res, result);    
+            res.redirect('/catalog');        
+        } catch (err) {
+            res.render('signUp', {
+                title: 'Sign up',
+                body: {
+                    username: req.body.username
+                },
+                error: parseError(error)
+            });
         }
-        if (req.body.password.trim() != req.body.repass.trim()) {
-            throw new Error('Passwords don\'t match!');
-        }
-        const result = await register(req.body.username.trim(), req.body.password.trim());    
-        attachToken(req, res, result);    
-        res.redirect('/catalog');        
-    } catch (err) {
-        res.render('signUp', {
-            title: 'Sign up',
-            error: err.message.split('\n')
-        });
     }
-});
+);
 
 authController.get('/signout', (req, res) => {
     res.clearCookie('jwt');
@@ -57,6 +87,5 @@ function attachToken(req, res, data) {
     const token = req.signJwt(data);
     res.cookie('jwt', token, { maxAge: 14400000 });
 }
-
 
 module.exports = authController;
